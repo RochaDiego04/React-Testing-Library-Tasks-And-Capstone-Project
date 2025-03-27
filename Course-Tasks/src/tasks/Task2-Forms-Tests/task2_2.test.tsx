70046602959e130c40320e47f284a86d26883d8b
Diego Rocha <drochaesc@gmail.com>
2025-03-27 12:06:57 -0600

    feat: task2 part 2 and part 3, image showing 100& coverage

--- Course-Tasks/src/tasks/Task2-Forms-Tests/task2_2.test.tsx
+++ Course-Tasks/src/tasks/Task2-Forms-Tests/task2_2.test.tsx
@@ -1,3 +1,16 @@
 import MessageForm from "./task2_2";
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 
-test("updates message on input change", () => {});
+test("updates message on input change", async () => {
+  render(<MessageForm />);
+
+  const input = screen.getByRole("textbox");
+
+  await userEvent.type(input, "hello");
+
+  expect(input).toHaveValue("hello");
+
+  // Displayed in the paragprah
+  expect(screen.getByText("Message: hello")).toBeInTheDocument();
+});
--- Course-Tasks/src/tasks/Task2-Forms-Tests/task2_3.test.tsx
+++ Course-Tasks/src/tasks/Task2-Forms-Tests/task2_3.test.tsx
@@ -1,9 +1,51 @@
+import userEvent from "@testing-library/user-event";
 import MouseEventTracker from "./task2_3";
+import { fireEvent, render, screen } from "@testing-library/react";
 
-test("updates message on single click", () => {});
+test("updates message on single click", async () => {
+  render(<MouseEventTracker />);
 
-test("updates message on double click", () => {});
+  const clickMessageParagraph = screen.getAllByRole("paragraph")[0];
+  expect(clickMessageParagraph).toBeEmptyDOMElement();
 
-test("updates message on mouse over", () => {});
+  const button = screen.getByRole("button", { name: /mouse event button/i });
 
-test("updates message on mouse out", () => {});
+  await userEvent.click(button);
+  expect(screen.getByText("Button Clicked!")).toBeInTheDocument();
+});
+
+test("updates message on double click", async () => {
+  render(<MouseEventTracker />);
+
+  const clickMessageParagraph = screen.getAllByRole("paragraph")[0];
+  expect(clickMessageParagraph).toBeEmptyDOMElement();
+
+  const button = screen.getByRole("button", { name: /mouse event button/i });
+
+  await userEvent.dblClick(button);
+  expect(screen.getByText("Button Double Clicked!")).toBeInTheDocument();
+});
+
+test("updates message on mouse over", () => {
+  render(<MouseEventTracker />);
+
+  const clickMessageParagraph = screen.getAllByRole("paragraph")[1];
+  expect(clickMessageParagraph).toBeEmptyDOMElement();
+
+  const button = screen.getByRole("button", { name: /mouse event button/i });
+  fireEvent.mouseOver(button);
+
+  expect(screen.getByText("Mouse Over Button")).toBeInTheDocument();
+});
+
+test("updates message on mouse out", () => {
+  render(<MouseEventTracker />);
+
+  const clickMessageParagraph = screen.getAllByRole("paragraph")[1];
+  expect(clickMessageParagraph).toBeEmptyDOMElement();
+
+  const button = screen.getByRole("button", { name: /mouse event button/i });
+  fireEvent.mouseLeave(button);
+
+  expect(screen.getByText("Mouse Left Button")).toBeInTheDocument();
+});
