ad98c4153522ee4d4166fc013d66d3d05922d350
Diego Rocha <drochaesc@gmail.com>
2025-03-27 11:36:34 -0600

    feat: task2 part 1, incrementing counter

--- Course-Tasks/src/tasks/Task2-Forms-Tests/task2_1.test.tsx
+++ Course-Tasks/src/tasks/Task2-Forms-Tests/task2_1.test.tsx
@@ -1,3 +1,15 @@
+import { render, screen } from "@testing-library/react";
 import Counter from "./task2_1";
+import userEvent from "@testing-library/user-event";
 
-test("increments counter on button click", () => {});
+test("increments counter on button click", async () => {
+  const user = userEvent.setup();
+  render(<Counter />);
+
+  expect(screen.getByText(/count: 0/i)).toBeInTheDocument();
+
+  const button = screen.getByRole("button", { name: /increment/i });
+  await user.click(button);
+
+  expect(screen.getByText(/count: 1/i)).toBeInTheDocument();
+});
